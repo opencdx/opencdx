98d485e668042a1758406e1939fc72b72b162760
Jeffery Miller <jeff.miller@safehealth.me>
2023-09-11 18:54:20 -0400

    OpenCDXAuditService

---
+++ opencdx-commons/src/main/java/health/safe/api/opencdx/commons/service/OpenCDXAuditService.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2023 Safe Health Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package health.safe.api.opencdx.commons.service;
+
+import java.util.UUID;
+
+/**
+ * Interface for implementing OpenCDXAuditService. Used to record audit events.
+ * An implementation agnostic interface.
+ */
+public interface OpenCDXAuditService {
+
+    /**
+     * OpenCDXMessage Subject for Audit messages.
+     */
+    public static final String AUDIT_MESSAGE_SUBJECT = "opencdx-audit-message";
+
+    /**
+     * Record User Login successful to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     */
+    void userLoginSucceed(UUID actor);
+
+    /**
+     * Record User Login failed to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     */
+    void userLoginFailure(UUID actor);
+
+    /**
+     * Record user Logout to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     */
+    void userLogout(UUID actor);
+
+    /**
+     * Record user acccess changed to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void userAccessChange(UUID actor, UUID auditEntity);
+
+    /**
+     * Record user password changed to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void passwordChange(UUID actor, UUID auditEntity);
+
+    /**
+     * Record User PII information accessed to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void piiAccessed(UUID actor, UUID auditEntity);
+
+    /**
+     * Record User PII information created to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void piiCreated(UUID actor, UUID auditEntity);
+
+    /**
+     * Record user PII information updated to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void piiUpdated(UUID actor, UUID auditEntity);
+
+    /**
+     * Record user PII Information deleted to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void piiDeleted(UUID actor, UUID auditEntity);
+
+    /**
+     * Record user PHI information accessed to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void phiAccessed(UUID actor, UUID auditEntity);
+
+    /**
+     * Record user PHI information created ot audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void phiCreated(UUID actor, UUID auditEntity);
+
+    /**
+     * Record User PHI information updated to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void phiUpdated(UUID actor, UUID auditEntity);
+
+    /**
+     * Record user PHI information deleted to audit log.
+     * @param actor Currently logged in user who initiated the actions being recorded.
+     * @param auditEntity User that data was accessed in the system or modified.
+     */
+    void phiDeleted(UUID actor, UUID auditEntity);
+}
---
+++ opencdx-commons/src/main/java/health/safe/api/opencdx/commons/service/impl/OpenCDXAuditServiceImpl.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright 2023 Safe Health Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package health.safe.api.opencdx.commons.service.impl;
+
+import com.google.protobuf.Timestamp;
+import health.safe.api.opencdx.commons.service.OpenCDXAuditService;
+import health.safe.api.opencdx.commons.service.OpenCDXMessageService;
+import health.safe.api.opencdx.grpc.audit.*;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * OpenCDX-Audit based implementation of OpenCDXAuditService
+ */
+@Slf4j
+@Service
+public class OpenCDXAuditServiceImpl implements OpenCDXAuditService {
+
+    private OpenCDXMessageService messageService;
+
+    private String applicationName;
+    /**
+     * Default Constructor
+     */
+    public OpenCDXAuditServiceImpl() {
+        // Explicit declaration to prevent this class from inadvertently being made instantiable
+    }
+    /**
+     * Constructor based on the OpenCDXMessageService
+     * @param messageService Messaging Service to use,
+     * @param applicationName Applicaiton name to set.
+     */
+    public OpenCDXAuditServiceImpl(
+            OpenCDXMessageService messageService, @Value("${spring.application.name}") String applicationName) {
+        this.messageService = messageService;
+        this.applicationName = applicationName;
+    }
+
+    @Override
+    public void userLoginSucceed(UUID actor) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.USER_LOGIN_SUCCEEDED)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .build());
+    }
+
+    @Override
+    public void userLoginFailure(UUID actor) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.USER_LOGIN_FAIL)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .build());
+    }
+
+    @Override
+    public void userLogout(UUID actor) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.USER_LOGOUT)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .build());
+    }
+
+    @Override
+    public void userAccessChange(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.USER_ACCESS_CHANGE)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    @Override
+    public void passwordChange(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.USER_PASSWORD_CHANGE)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    @Override
+    public void piiAccessed(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.PII_ACCESSED)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    @Override
+    public void piiCreated(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.PII_CREATED)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    @Override
+    public void piiUpdated(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.PII_UPDATED)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    @Override
+    public void piiDeleted(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.PII_DELETED)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    @Override
+    public void phiAccessed(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.PHI_ACCESSED)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    @Override
+    public void phiCreated(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.PHI_CREATED)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    @Override
+    public void phiUpdated(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.PHI_UPDATED)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    @Override
+    public void phiDeleted(UUID actor, UUID auditEntity) {
+        this.sendMessage(AuditEvent.newBuilder()
+                .setEventType(AuditEventType.PHI_DELETED)
+                .setCreated(this.getTimeStamp(Instant.now()))
+                .setAuditSource(this.getAuditSource(this.applicationName))
+                .setActor(this.getActor(actor))
+                .setAuditEntity(this.getAuditEntity(auditEntity))
+                .build());
+    }
+
+    private void sendMessage(AuditEvent event) {
+        this.messageService.send(OpenCDXAuditService.AUDIT_MESSAGE_SUBJECT, event);
+    }
+
+    private Timestamp getTimeStamp(Instant time) {
+        return Timestamp.newBuilder()
+                .setSeconds(time.getEpochSecond())
+                .setNanos(time.getNano())
+                .build();
+    }
+
+    private AuditSource getAuditSource(String applicationName) {
+        return AuditSource.newBuilder().setSystemInfo(applicationName).build();
+    }
+
+    private Actor getActor(UUID actor) {
+        return Actor.newBuilder().setIdentity(actor.toString()).build();
+    }
+
+    private AuditEntity getAuditEntity(UUID auditEntity) {
+        return AuditEntity.newBuilder()
+                .setPatientIdentifier(auditEntity.toString())
+                .build();
+    }
+}
