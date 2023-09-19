632420728ed8cdefbe4e5f23f9e57fa51222ef4f
Bill Habermaas <bill@safehealth.me>
2023-09-19 12:13:40 -0400

    Clean build without errors

--- opencdx-commons/src/main/java/health/safe/api/opencdx/commons/aspects/AuditAspect.java
+++ opencdx-commons/src/main/java/health/safe/api/opencdx/commons/aspects/AuditAspect.java
@@ -15,7 +15,6 @@
  */
 package health.safe.api.opencdx.commons.aspects;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import health.safe.api.opencdx.commons.dto.RequestActorAttributes;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -27,30 +26,26 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
 @EnableAspectJAutoProxy
 @Component
+@SuppressWarnings("java:S1874")
 public class AuditAspect {
-
-    private final ObjectMapper objectMapper;
-    private final ExpressionParser parser;
-
     private static final ConcurrentMap<Long, RequestActorAttributes> userInfo = new ConcurrentHashMap<>();
-
-    public AuditAspect() {
-
-        this.objectMapper = new ObjectMapper();
-        this.parser = new SpelExpressionParser();
-    }
 
     @Order(Ordered.LOWEST_PRECEDENCE)
     @Around(value = "@annotation(health.safe.api.opencdx.commons.annotations.OpenCDXAuditUser)")
-    public void auditUser(ProceedingJoinPoint proceedingJoinPoint) {}
+    public void auditUser(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        try {
+            proceedingJoinPoint.proceed();
+        } finally {
+            // clear
+            removeCurrentThreadInfo();
+        }
+    }
 
     public static RequestActorAttributes getCurrentThreadInfo() {
         log.debug("Clearing Current Thread: {}", Thread.currentThread().getName());
