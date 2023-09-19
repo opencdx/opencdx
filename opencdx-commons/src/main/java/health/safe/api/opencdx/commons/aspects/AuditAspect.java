f41e54be903624e70f8b93930b42af41b765337a
Bill Habermaas <bill@safehealth.me>
2023-09-19 10:04:47 -0400

    save latest changes

--- opencdx-commons/src/main/java/health/safe/api/opencdx/commons/aspects/AuditAspect.java
+++ opencdx-commons/src/main/java/health/safe/api/opencdx/commons/aspects/AuditAspect.java
@@ -1,22 +1,35 @@
+/*
+ * Copyright 2023 Safe Health Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package health.safe.api.opencdx.commons.aspects;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import health.safe.api.opencdx.commons.dto.RequestActorAttributes;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.Joinpoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.Ordered;
-
+import org.springframework.core.annotation.Order;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 @Slf4j
 @Aspect
@@ -27,8 +40,7 @@ public class AuditAspect {
     private final ObjectMapper objectMapper;
     private final ExpressionParser parser;
 
-    private static final ConcurrentMap<Long, RequestActorAttributes> userInfo =
-            new ConcurrentHashMap<>();
+    private static final ConcurrentMap<Long, RequestActorAttributes> userInfo = new ConcurrentHashMap<>();
 
     public AuditAspect() {
         this.objectMapper = new ObjectMapper();
@@ -36,9 +48,8 @@ public class AuditAspect {
     }
 
     @Order(Ordered.LOWEST_PRECEDENCE)
-    //@Around(value = "annotation()")
-    public void auditUser(Joinpoint joinPoint) {
-    }
+    @Around(value = "annotation()")
+    public void auditUser(Joinpoint joinPoint) {}
 
     public static RequestActorAttributes getCurrentThreadInfo() {
         log.debug("Clearing Current Thread: {}", Thread.currentThread().getName());
@@ -46,8 +57,11 @@ public class AuditAspect {
     }
 
     public static void setCurrentThreadInfo(UUID actor, UUID patient) {
-        log.debug("Thread: {} being set with actor {}, patient {}",
-                Thread.currentThread().getName(), actor.toString(), patient.toString());
+        log.debug(
+                "Thread: {} being set with actor {}, patient {}",
+                Thread.currentThread().getName(),
+                actor.toString(),
+                patient.toString());
         userInfo.put(Thread.currentThread().getId(), new RequestActorAttributes(actor, patient));
     }
 
