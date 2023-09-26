0fe61f34b82bb6815f03cc315d3b39a1b3e76bf8
Jeffery Miller <jeff.miller@safehealth.me>
2023-09-26 16:11:12 -0400

    Updated for Communications

---
+++ opencdx-communications/src/main/java/health/safe/api/opencdx/communications/model/OpenCDXNotification.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2023 Safe Health Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package health.safe.api.opencdx.communications.model;
+
+import cdx.open_communication.v2alpha.Attachment;
+import cdx.open_communication.v2alpha.Notification;
+import cdx.open_communication.v2alpha.NotificationStatus;
+import com.google.protobuf.Timestamp;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Document("notifications")
+public class OpenCDXNotification {
+    @Id
+    private ObjectId id;
+
+    private ObjectId eventId;
+    private NotificationStatus smsStatus;
+    private NotificationStatus emailStatus;
+    private ZonedDateTime timestamp;
+    private Map<String, String> customData;
+    private List<String> toEmail;
+    private List<String> ccEmail;
+    private List<String> bccEmail;
+    private List<Attachment> attachments;
+    private List<String> phoneNumbers;
+    private Map<String, String> variables;
+    private List<ObjectId> recipients;
+
+    private Integer failed;
+
+    public OpenCDXNotification(Notification notification) {
+        if (notification.hasQueueId()) {
+            this.id = new ObjectId(notification.getEventId());
+        }
+        this.eventId = new ObjectId(notification.getEventId());
+        if (notification.hasSmsStatus()) {
+            this.smsStatus = notification.getSmsStatus();
+        } else {
+            this.smsStatus = NotificationStatus.NOTIFICATION_STATUS_PENDING;
+        }
+        if (notification.hasEmailStatus()) {
+            this.emailStatus = notification.getEmailStatus();
+        } else {
+            this.emailStatus = NotificationStatus.NOTIFICATION_STATUS_PENDING;
+        }
+        this.timestamp = Instant.ofEpochSecond(
+                        notification.getTimestamp().getSeconds(),
+                        notification.getTimestamp().getNanos())
+                .atZone(ZoneId.systemDefault());
+
+        this.customData = notification.getCustomDataMap();
+        this.toEmail = notification.getToEmailList();
+        this.ccEmail = notification.getCcEmailList();
+        this.bccEmail = notification.getBccEmailList();
+        this.attachments = notification.getEmailAttachmentsList();
+        this.phoneNumbers = notification.getToPhoneNumberList();
+        this.variables = notification.getVariablesMap();
+        this.recipients =
+                notification.getRecipientsIdList().stream().map(ObjectId::new).toList();
+    }
+
+    public Notification getProtobufMessage() {
+        return Notification.newBuilder()
+                .setQueueId(this.id.toHexString())
+                .setEventId(this.eventId.toHexString())
+                .setSmsStatus(this.smsStatus)
+                .setEmailStatus(this.emailStatus)
+                .setTimestamp(Timestamp.newBuilder()
+                        .setSeconds(this.timestamp.toEpochSecond())
+                        .setNanos(this.timestamp.getNano())
+                        .build())
+                .putAllCustomData(this.customData)
+                .addAllToEmail(this.toEmail)
+                .addAllCcEmail(this.ccEmail)
+                .addAllBccEmail(this.bccEmail)
+                .addAllEmailAttachments(this.attachments)
+                .addAllToPhoneNumber(this.phoneNumbers)
+                .putAllVariables(this.variables)
+                .addAllRecipientsId(
+                        this.recipients.stream().map(ObjectId::toHexString).toList())
+                .build();
+    }
+}
