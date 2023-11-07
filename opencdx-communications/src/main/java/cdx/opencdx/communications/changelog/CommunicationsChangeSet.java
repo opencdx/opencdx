bc32103a54e6342383ce3b0676b1345714ac8e46
Gaurav Mishra <gaurav.mishra@safehealth.me>
2023-11-07 18:18:13 +0530

    Thymeleaf substitution fix

--- opencdx-communications/src/main/java/cdx/opencdx/communications/changelog/CommunicationsChangeSet.java
+++ opencdx-communications/src/main/java/cdx/opencdx/communications/changelog/CommunicationsChangeSet.java
@@ -331,10 +331,10 @@ public class CommunicationsChangeSet {
                 .subject("OpenCDX Notification")
                 .content(
                         """
-                        Dear ${firstName} ${lastName},
+                        Dear [[${firstName}] [[${lastName}]],
 
-                        To verify your email : ${email} click the link below :
-                        <a href="${verification_server}/${user_id}" target="_blank">${verification_server}/${user_id}</a>
+                        To verify your email : [[${email}]] click the link below :
+                        <a href="@{|${verification_server}/${user_id}|}" target="_blank">[[${verification_server}]]/[[${user_id}]]</a>
 
                         Thank you!
                         """)
