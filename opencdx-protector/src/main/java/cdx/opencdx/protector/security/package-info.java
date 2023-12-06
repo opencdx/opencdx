33f8c2f84a51a0c78fc8b8fa4cae12de11f3afd5
Ravi Singh <ravi@safehealth.me>
2023-12-06 14:29:31 -0500

    protector service

---
+++ opencdx-protector/src/main/java/cdx/opencdx/protector/security/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package is for security related configuration for spring
+ * <p>
+ * Spring Security related files are seperated as a whole for easier identification and lookup.
+ * </p>
+ *
+ * @author Safe Health LLC.
+ * @version 1.0
+ * @since 1.0
+ */
+package cdx.opencdx.protector.security;
