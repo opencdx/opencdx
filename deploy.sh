b034ba1772633a563fe5f42b39f1ba446f2dafbc
Jeffery Miller <jeff.miller@safehealth.me>
2023-12-06 16:47:58 -0500

    Deploy changes

--- deploy.sh
+++ deploy.sh
@@ -243,10 +243,11 @@ build_docker() {
     build_docker_image opencdx/media ./opencdx-media
     build_docker_image opencdx/connected-test ./opencdx-connected-test
     build_docker_image opencdx/iam ./opencdx-iam
-	build_docker_image opencdx/routine ./opencdx-routine 
+	  build_docker_image opencdx/routine ./opencdx-routine
     build_docker_image opencdx/gateway ./opencdx-gateway
     build_docker_image opencdx/discovery ./opencdx-discovery
     build_docker_image opencdx/frontend ./opencdx-frontend
+    build_docker_image opencdx/protector ./opencdx-protector
 }
 
 # Function to start Docker services
