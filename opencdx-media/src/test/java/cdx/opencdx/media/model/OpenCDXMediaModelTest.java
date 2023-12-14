551ca3d77f44ab9357ae5a2afeca3e50922be9d2
Jeffery Miller <jeff.miller@safehealth.me>
2023-12-14 13:51:38 -0500

    Media

--- opencdx-media/src/test/java/cdx/opencdx/media/model/OpenCDXMediaModelTest.java
+++ opencdx-media/src/test/java/cdx/opencdx/media/model/OpenCDXMediaModelTest.java
@@ -29,8 +29,8 @@ class OpenCDXMediaModelTest {
                 .setId(ObjectId.get().toHexString())
                 .setCreatedAt(Timestamp.getDefaultInstance())
                 .setUpdatedAt(Timestamp.getDefaultInstance())
-                .setOrganizationSlug("organization")
-                .setWorkspaceSlug("workspace")
+                .setOrganizationId(ObjectId.get().toHexString())
+                .setWorkspaceId(ObjectId.get().toHexString())
                 .setName("name")
                 .setShortDescription("This is a short Description")
                 .setDescription("This is a description")
@@ -54,8 +54,8 @@ class OpenCDXMediaModelTest {
     @Test
     void getProtobufMessage_2() {
         OpenCDXMediaModel model = new OpenCDXMediaModel(Media.newBuilder()
-                .setOrganizationSlug("organization")
-                .setWorkspaceSlug("workspace")
+                .setOrganizationId(ObjectId.get().toHexString())
+                .setWorkspaceId(ObjectId.get().toHexString())
                 .setName("name")
                 .setShortDescription("This is a short Description")
                 .setDescription("This is a description")
