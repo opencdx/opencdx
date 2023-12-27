8e71bb84d1a037b26bf5a163721dc8bf341c696b
Ravi Singh <ravi@safehealth.me>
2023-12-27 15:40:57 -0500

    compairson

--- opencdx-dashboard/src/pages/ExternalInterfaces.js
+++ opencdx-dashboard/src/pages/ExternalInterfaces.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { Typography } from '@mui/material';
-import MainCard from 'ui-component/cards/MainCard';
+import { diffChars } from 'diff';
 
-const ExternalInterfaces = () => {
-    // Your component logic goes here
+const FileComparison = () => {
+  const oldJson = `{
+  "key1": "value1",
+  "key2": "value2",
+  "key4": "value4"
+}`;
 
-    return (
-        <MainCard title="External Interfaces">
-            <Typography variant="body2">External Interfaces</Typography>
-        </MainCard>
-    );
+  const newJson = `{
+  "key1": "updatedValue1",
+  "key2": "value2",
+  "key3": "value3"
+}`;
+
+  const differences = diffChars(oldJson, newJson);
+
+  return (
+    <div>
+      {differences.map((part, index) => (
+        <span
+          key={index}
+          style={
+            part.added
+              ? { background: 'lightgreen' }
+              : part.removed
+              ? { background: 'lightcoral' }
+              : null
+          }
+        >
+          {part.value.split('\n').map((line, lineIndex) => (
+            <React.Fragment key={lineIndex}>
+              {lineIndex > 0 && <br />} {line}
+            </React.Fragment>
+          ))}
+        </span>
+      ))}
+    </div>
+  );
 };
 
-export default ExternalInterfaces;
+export default FileComparison;
