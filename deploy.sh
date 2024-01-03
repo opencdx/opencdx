2de97b19808d801fd96ffb66a2e79ae08b054df1
Jeffery Miller <jeff.miller@safehealth.me>
2024-01-03 16:48:49 -0500

    Update

--- deploy.sh
+++ deploy.sh
@@ -302,15 +302,17 @@ build_docker() {
     build_docker_image opencdx/media ./opencdx-media
     build_docker_image opencdx/connected-test ./opencdx-connected-test
     build_docker_image opencdx/iam ./opencdx-iam
-	build_docker_image opencdx/routine ./opencdx-routine
+	  build_docker_image opencdx/routine ./opencdx-routine
     build_docker_image opencdx/protector ./opencdx-protector
     build_docker_image opencdx/predictor ./opencdx-predictor
     build_docker_image opencdx/questionnaire ./opencdx-questionnaire
     build_docker_image opencdx/classification ./opencdx-classification
     build_docker_image opencdx/gateway ./opencdx-gateway
     build_docker_image opencdx/discovery ./opencdx-discovery
-    build_docker_image opencdx/dashboard ./opencdx-dashboard
     build_docker_image opencdx/anf ./opencdx-anf
+    if [ "$no_ui" = false ]; then
+      build_docker_image opencdx/dashboard ./opencdx-dashboard
+    fi
 }
 
 # Function to start Docker services
