55b59f525231eb555d038e229bfcce915bbad6d2
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2024-01-10 13:38:06 -0500

    Don't use scroll view on web

--- opencdx-form-render/App.js
+++ opencdx-form-render/App.js
@@ -1,12 +1,22 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import Constants from 'expo-constants';
 import FormRender from './components/FormRender';
+import { GluestackUIProvider, ScrollView } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <FormRender/>
+        <GluestackUIProvider config={config}>
+            { Platform.OS === "web" ? (
+              <FormRender/>
+            ) : (
+              <ScrollView h="$80" w="$100">
+                <FormRender/>
+              </ScrollView>
+            )}
+      </GluestackUIProvider>
     </View>
   );
 }
