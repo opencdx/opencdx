46976d9aabfe9cbd698f265b3a0e1c1115bb1ef2
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2024-01-10 12:14:58 -0500

    Use form builder json

--- opencdx-form-render/App.js
+++ opencdx-form-render/App.js
@@ -1,168 +1,17 @@
 import { StyleSheet, View } from 'react-native';
 import Constants from 'expo-constants';
-import { useState } from 'react';
-import { GluestackUIProvider, Button, ButtonText, Text, Input, InputField, Radio, RadioGroup, RadioIndicator, RadioIcon, CircleIcon, HStack, RadioLabel, Select, SelectTrigger, SelectItem, SelectIcon, SelectContent, SelectPortal, SelectDragIndicatorWrapper, SelectBackdrop, SelectInput, Icon, ChevronDownIcon, SelectDragIndicator, FormControl, FormControlLabel, FormControlLabelText } from '@gluestack-ui/themed';
-import { config } from '@gluestack-ui/config';
+import FormRender from './components/FormRender';
 
 export default function App() {
-  const [cuff, setCuff] = useState();
-  const [urinated, setUrinated] = useState();
-  const [sitting, setSitting] = useState();
 
   return (
     <View style={styles.container}>
-      <GluestackUIProvider config={config}>
-        <FormControl minWidth="$80">
-        <FormControlLabel>
-          <FormControlLabelText>Upper Range (SYSTOLIC)</FormControlLabelText>
-        </FormControlLabel>
-        <Input
-          style={styles.margin}
-          variant="outline"
-          size="md"
-          isDisabled={false}
-          isInvalid={false}
-          isReadOnly={false}
-        >
-          <InputField placeholder="Enter Text here" />
-        </Input>
-
-        <FormControlLabel>
-          <FormControlLabelText>Measurement taken using cuff</FormControlLabelText>
-        </FormControlLabel>
-        <RadioGroup value={cuff} onChange={setCuff} style={styles.margin}>
-          <HStack space="2xl">
-            <Radio value="yes">
-              <RadioIndicator mr="$2">
-                <RadioIcon as={CircleIcon} />
-              </RadioIndicator>
-              <RadioLabel>Yes</RadioLabel>
-            </Radio>
-            <Radio value="no">
-              <RadioIndicator mr="$2">
-                <RadioIcon as={CircleIcon} />
-              </RadioIndicator>
-              <RadioLabel>No</RadioLabel>
-            </Radio>
-            <Radio value="notAnswered">
-              <RadioIndicator mr="$2">
-                <RadioIcon as={CircleIcon} />
-              </RadioIndicator>
-              <RadioLabel>Not Answered</RadioLabel>
-            </Radio>
-          </HStack>
-        </RadioGroup>
-
-        <FormControlLabel>
-          <FormControlLabelText>Cuff Size</FormControlLabelText>
-        </FormControlLabel>
-        <Select style={styles.margin}>
-          <SelectTrigger variant="outline" size="md">
-            <SelectInput placeholder="Select option" />
-            <SelectIcon mr="$3">
-              <Icon as={ChevronDownIcon} />
-            </SelectIcon>
-          </SelectTrigger>
-          <SelectPortal>
-            <SelectBackdrop />
-            <SelectContent>
-              <SelectDragIndicatorWrapper>
-                <SelectDragIndicator />
-              </SelectDragIndicatorWrapper>
-              <SelectItem label="Adult" value="adult" />
-              <SelectItem label="Pediatric" value="pediatric" />
-            </SelectContent>
-          </SelectPortal>
-        </Select>
-
-        <FormControlLabel>
-          <FormControlLabelText>Arm Used</FormControlLabelText>
-        </FormControlLabel>
-        <Select style={styles.margin}>
-          <SelectTrigger variant="outline" size="md">
-            <SelectInput placeholder="Select option" />
-            <SelectIcon mr="$3">
-              <Icon as={ChevronDownIcon} />
-            </SelectIcon>
-          </SelectTrigger>
-          <SelectPortal>
-            <SelectBackdrop />
-            <SelectContent>
-              <SelectDragIndicatorWrapper>
-                <SelectDragIndicator />
-              </SelectDragIndicatorWrapper>
-              <SelectItem label="Right" value="right" />
-              <SelectItem label="Left" value="left" />
-            </SelectContent>
-          </SelectPortal>
-        </Select>
-
-        <FormControlLabel>
-          <FormControlLabelText>Please confirm you have not urinated more than 30 minutes prior to taking the measurement.</FormControlLabelText>
-        </FormControlLabel>
-        <RadioGroup value={urinated} onChange={setUrinated} style={styles.margin}>
-          <HStack space="2xl">
-            <Radio value="yes">
-              <RadioIndicator mr="$2">
-                <RadioIcon as={CircleIcon} />
-              </RadioIndicator>
-              <RadioLabel>Yes</RadioLabel>
-            </Radio>
-            <Radio value="no">
-              <RadioIndicator mr="$2">
-                <RadioIcon as={CircleIcon} />
-              </RadioIndicator>
-              <RadioLabel>No</RadioLabel>
-            </Radio>
-            <Radio value="notAnswered">
-              <RadioIndicator mr="$2">
-                <RadioIcon as={CircleIcon} />
-              </RadioIndicator>
-              <RadioLabel>Not Answered</RadioLabel>
-            </Radio>
-          </HStack>
-        </RadioGroup>
-
-        <FormControlLabel>
-          <FormControlLabelText>Please confirm you have been in sitting position before taking the measurement for at least 5 minutes.</FormControlLabelText>
-        </FormControlLabel>
-        <RadioGroup value={sitting} onChange={setSitting} style={styles.margin}>
-          <HStack space="2xl">
-            <Radio value="yes">
-              <RadioIndicator mr="$2">
-                <RadioIcon as={CircleIcon} />
-              </RadioIndicator>
-              <RadioLabel>Yes</RadioLabel>
-            </Radio>
-            <Radio value="no">
-              <RadioIndicator mr="$2">
-                <RadioIcon as={CircleIcon} />
-              </RadioIndicator>
-              <RadioLabel>No</RadioLabel>
-            </Radio>
-            <Radio value="notAnswered">
-              <RadioIndicator mr="$2">
-                <RadioIcon as={CircleIcon} />
-              </RadioIndicator>
-              <RadioLabel>Not Answered</RadioLabel>
-            </Radio>
-          </HStack>
-        </RadioGroup>
-
-        <Button style={styles.margin}>
-          <ButtonText>Submit</ButtonText>
-        </Button>
-        </FormControl>
-      </GluestackUIProvider>
+      <FormRender/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  margin: {
-    margin: 5,
-    marginLeft: 0,
-  },
   container: {
     flex: 1,
     justifyContent: 'center',
---
+++ opencdx-form-render/components/FormRender.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { GluestackUIProvider, Button, ButtonText, Text, Input, InputField, Radio, RadioGroup, RadioIndicator, RadioIcon, CircleIcon, HStack, RadioLabel, Select, SelectTrigger, SelectItem, SelectIcon, SelectContent, SelectPortal, SelectDragIndicatorWrapper, SelectBackdrop, SelectInput, Icon, ChevronDownIcon, SelectDragIndicator, FormControl, FormControlLabel, FormControlLabelText, ScrollView } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
+import formData from './alpha.json';
+
+const FormRender = React.forwardRef((props, ref) => {
+    const [cuff, setCuff] = useState();
+    const [urinated, setUrinated] = useState();
+    const [sitting, setSitting] = useState();
+
+    return(
+        <GluestackUIProvider config={config}>
+            <ScrollView h="$80" w="$100">
+            <FormControl minWidth="$80">
+                { formData?.item?.map((question, index) => {
+                    return(
+                        <>
+                        <FormControlLabel>
+                            <FormControlLabelText>{question.text}</FormControlLabelText>
+                        </FormControlLabel>
+
+                        { question.type === "integer" &&
+                            <Input
+                                style={styles.margin}
+                                variant="outline"
+                                size="md"
+                                isDisabled={false}
+                                isInvalid={false}
+                                isReadOnly={false}
+                            >
+                                <InputField placeholder="Enter Text here" />
+                            </Input>
+                        }
+
+                        { question.type === "string" &&
+                            <Input
+                                style={styles.margin}
+                                variant="outline"
+                                size="md"
+                                isDisabled={false}
+                                isInvalid={false}
+                                isReadOnly={false}
+                            >
+                                <InputField placeholder="Enter Text here" />
+                            </Input>
+                        }
+
+                        { question.type === "text" &&
+                            <Input
+                                style={styles.margin}
+                                variant="outline"
+                                size="md"
+                                isDisabled={false}
+                                isInvalid={false}
+                                isReadOnly={false}
+                            >
+                                <InputField placeholder="Enter Text here" />
+                            </Input>
+                        }
+
+                        { question.type === "boolean" &&
+                            <RadioGroup value={cuff} onChange={setCuff} style={styles.margin}>
+                                <HStack space="2xl">
+                                    <Radio value="yes">
+                                    <RadioIndicator mr="$2">
+                                        <RadioIcon as={CircleIcon} />
+                                    </RadioIndicator>
+                                    <RadioLabel>Yes</RadioLabel>
+                                    </Radio>
+                                    <Radio value="no">
+                                    <RadioIndicator mr="$2">
+                                        <RadioIcon as={CircleIcon} />
+                                    </RadioIndicator>
+                                    <RadioLabel>No</RadioLabel>
+                                    </Radio>
+                                    <Radio value="notAnswered">
+                                    <RadioIndicator mr="$2">
+                                        <RadioIcon as={CircleIcon} />
+                                    </RadioIndicator>
+                                    <RadioLabel>Not Answered</RadioLabel>
+                                    </Radio>
+                                </HStack>
+                            </RadioGroup>
+                        }
+
+                        { question.type === "choice" &&
+                            <Select style={styles.margin}>
+                                <SelectTrigger variant="outline" size="md">
+                                    <SelectInput placeholder="Select option" />
+                                    <SelectIcon mr="$3">
+                                    <Icon as={ChevronDownIcon} />
+                                    </SelectIcon>
+                                </SelectTrigger>
+                                <SelectPortal>
+                                    <SelectBackdrop />
+                                    <SelectContent>
+                                    <SelectDragIndicatorWrapper>
+                                        <SelectDragIndicator />
+                                    </SelectDragIndicatorWrapper>
+                                    {question.answerOption.map((option) => {
+                                        return(
+                                            <SelectItem label={option.valueCoding.display} value={option.valueCoding.display} />
+                                        )
+                                    })}
+                                    </SelectContent>
+                                </SelectPortal>
+                            </Select>
+                        }
+                        </>
+                    )
+                }) }
+
+                <Button style={styles.margin}>
+                <ButtonText>Submit</ButtonText>
+                </Button>
+            </FormControl>
+            </ScrollView>
+        </GluestackUIProvider>
+    );
+});
+
+const styles = StyleSheet.create({
+    margin: {
+        margin: 5,
+        marginLeft: 0,
+    },
+});
+
+export default FormRender;
