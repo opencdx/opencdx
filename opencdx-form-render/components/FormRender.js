e3653c4e68cf75764d4fd2c1b84d65cfc957e88f
Karthick Rajamurugan <karthick.rajamurugan@safehealth.me>
2024-01-13 01:43:37 +0530

    Save functionality

--- opencdx-form-render/components/FormRender.js
+++ opencdx-form-render/components/FormRender.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { Button, ButtonText, Input, InputField, Radio, RadioGroup, RadioIndicator, RadioIcon, CircleIcon, HStack, RadioLabel, Select, SelectTrigger, SelectItem, SelectIcon, SelectContent, SelectPortal, SelectDragIndicatorWrapper, SelectBackdrop, SelectInput, Icon, ChevronDownIcon, SelectDragIndicator, FormControl, FormControlLabel, FormControlLabelText, Heading } from '@gluestack-ui/themed';
+import React, { useState } from 'react';
+import { TextInput } from './TextInput'
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Radio, RadioGroup, HStack, RadioIcon, CircleIcon, RadioLabel, RadioIndicator, Select, SelectTrigger, SelectInput, SelectIcon, SelectPortal, SelectBackdrop, SelectContent, SelectDragIndicatorWrapper, SelectDragIndicator, SelectItem, Icon, ChevronDownIcon, FormControlLabel, FormControlLabelText
+    
+ } from '@gluestack-ui/themed';
+import { useForm, FormProvider } from 'react-hook-form';
+import Constants from 'expo-constants';
 import formData from './alpha.json';
 
-const FormRender = React.forwardRef((props, ref) => {
-    const [cuff, setCuff] = useState();
-    const [urinated, setUrinated] = useState();
-    const [sitting, setSitting] = useState();
+
 const answerOption = [
     {
         id: 1,
@@ -25,120 +26,144 @@ const answerOption = [
         value: 'not answered',
     }
 ];
-    
-    return(
-        <FormControl minWidth="$80">
-            <Heading>{formData?.title}</Heading>
-            { formData?.item?.map((question, index) => {
-                return(
-                    <>
-                    <FormControlLabel>
-                        <FormControlLabelText>{question.text}</FormControlLabelText>
-                    </FormControlLabel>
+export default function App() {
+    const { ...methods } = useForm({ mode: 'onChange' });
 
-                    { question.type === "integer" &&
-                        <Input
-                            style={styles.margin}
-                            variant="outline"
-                            size="md"
-                            isDisabled={false}
-                            isInvalid={false}
-                            isReadOnly={false}
-                        >
-                            <InputField placeholder="Enter Text here" keyboardType="numeric"/>
-                        </Input>
-                    }
+    const onSubmit = (data) => console.log({ data });
 
-                    { question.type === "string" &&
-                        <Input
-                            style={styles.margin}
-                            variant="outline"
-                            size="md"
-                            isDisabled={false}
-                            isInvalid={false}
-                            isReadOnly={false}
-                        >
-                            <InputField placeholder="Enter Text here" />
-                        </Input>
-                    }
+    const [formError, setError] = useState(false)
 
-                    { question.type === "text" &&
-                        <Input
-                            style={styles.margin}
-                            variant="outline"
-                            size="md"
-                            isDisabled={false}
-                            isInvalid={false}
-                            isReadOnly={false}
-                        >
-                            <InputField placeholder="Enter Text here" />
-                        </Input>
-                    }
+    return (
+        <View style={styles.container}>
+            {formError ? <View><Text style={{ color: 'red' }}>There was a problem with loading the form. Please try again later.</Text></View> :
+                <>
+                    <FormProvider {...methods}>
+                        <Text>{formData?.title}</Text>
+                        {formData?.item?.map((field, index) => {
+                        
+                            return (
+                                <View>
+                                        <FormControlLabel>
+                                            <FormControlLabelText>{field.text}</FormControlLabelText>
+                                        </FormControlLabel>
+                                        {
+                                            field.type === "integer" && (
+                                    
+                                                <TextInput
+                                                    key={index}
+                                                    name={field.linkId}
+                                                    label={field.text}
+                                                    rules={{ required: 'This field is required!' }}
+                                                    setFormError={setError}
+                                                    type="number"
+                                                />
+                                            )
+                                            ||
+                                            field.type === "string" && (
+                                                <TextInput
+                                                    key={index}
+                                                    name={field.linkId}
+                                                    label={field.text}
+                                                    rules={{ required: 'This field is required!' }}
+                                                    setFormError={setError}
+                                                    type="text"
+                                                />
+                                            ) ||
+                                            field.type === "text" && (
+                                                <TextInput
+                                                    key={index}
+                                                    name={field.linkId}
+                                                    label={field.text}
+                                                    rules={{ required: 'This field is required!' }}
+                                                    setFormError={setError}
+                                                    type="text"
+                                                />
+                                            ) ||
+                                            field.type === "boolean" && (
+                                                <HStack style={styles.margin}>
+                                                    <RadioGroup
+                                                        key={index}
+                                                        name={field.linkId}
+                                                        flexDirection="row"
+            
+                                                        sx={{
+                                                            flexDirection: 'row',
+                                                        }}
+                                                        value={field.value}
+                                                        onValueChange={(value) => {
+                                                            // Handle radio group value change here
+                                                            // You can use the 'value' to update the state or perform any other logic
+                                                            console.log(value);
+                                                        }}
+                                                    >
+                                                        {answerOption?.map((option) => (
+                                                            <Radio key={option.id} value={option.value}>
+                                                                <RadioIndicator _checked={{ bg: 'primary.500' }} margin={2}>
+                                                                    <RadioIcon as={CircleIcon} />
+                                                                </RadioIndicator>
+                                                                <RadioLabel>{option.label}</RadioLabel>
+                                                            </Radio>
+                                                        ))}
+                                                    </RadioGroup>
+                                                </HStack>
+                                            ) ||
+                                            field.type === "choice" && (
+                                                <Select style={styles.margin}>
+                                                    <SelectTrigger variant="outline" size="md">
+                                                        <SelectInput placeholder="Select option" />
+                                                        <SelectIcon mr="$3">
+                                                            <Icon as={ChevronDownIcon} />
+                                                        </SelectIcon>
+                                                    </SelectTrigger>
+                                                    <SelectPortal>
+                                                        <SelectBackdrop />
+                                                        <SelectContent>
+                                                            <SelectDragIndicatorWrapper>
+                                                                <SelectDragIndicator />
+                                                            </SelectDragIndicatorWrapper>
+                                                            {field.answerOption.map((option) => {
+                                                                return (
+                                                                    <SelectItem label={option.valueCoding.display} value={option.valueCoding.display} />
+                                                                )
+                                                            })}
+                                                        </SelectContent>
+                                                    </SelectPortal>
+                                                </Select>
+                                            )
+                                        }
+                                        
+                                        </View>
+                                
+                            );
+                        }
+                        )}
 
-                    { question.type === "boolean" &&
-                        <HStack style={styles.margin}>
-                            <RadioGroup
-                            sx={{
-                                flexDirection: 'row',
-                            }}
-                                value={question.value}
-                                onValueChange={(value) => {
-                                    // Handle radio group value change here
-                                    // You can use the 'value' to update the state or perform any other logic
-                                    console.log(value);
-                                }}
-                            >
-                                {answerOption?.map((option) => (
-                                    <Radio key={option.id} value={option.value}>
-                                        <RadioIndicator _checked={{ bg: 'primary.500' }}  margin={2}>
-                                            <RadioIcon as={CircleIcon} />
-                                        </RadioIndicator>
-                                        <RadioLabel>{option.label}</RadioLabel>
-                                    </Radio>
-                                ))}
-                            </RadioGroup>
-                        </HStack>
-                    }
-
-                    { question.type === "choice" &&
-                        <Select style={styles.margin}>
-                            <SelectTrigger variant="outline" size="md">
-                                <SelectInput placeholder="Select option" />
-                                <SelectIcon mr="$3">
-                                <Icon as={ChevronDownIcon} />
-                                </SelectIcon>
-                            </SelectTrigger>
-                            <SelectPortal>
-                                <SelectBackdrop />
-                                <SelectContent>
-                                <SelectDragIndicatorWrapper>
-                                    <SelectDragIndicator />
-                                </SelectDragIndicatorWrapper>
-                                {question.answerOption.map((option) => {
-                                    return(
-                                        <SelectItem label={option.valueCoding.display} value={option.valueCoding.display} />
-                                    )
-                                })}
-                                </SelectContent>
-                            </SelectPortal>
-                        </Select>
-                    }
-                    </>
-                )
-            }) }
-
-            <Button style={styles.margin}>
-            <ButtonText>Submit</ButtonText>
-            </Button>
-        </FormControl>
+                    </FormProvider>
+                </>
+            }
+            <View style={styles.button}>
+                <Button
+                    title="Login"
+                    onPress={methods.handleSubmit(onSubmit)}
+                />
+            </View>
+        </View>
     );
-});
+}
 
 const styles = StyleSheet.create({
+    button: {
+        marginTop: 40,
+        color: 'white',
+        borderRadius: 4,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+    },
     margin: {
         margin: 5,
         marginLeft: 0,
     },
 });
-
-export default FormRender;
---
+++ opencdx-form-render/components/TextInput.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+import { View, TextInput as RNTextInput, TextInputProps as RNTextInputProps, Text, StyleSheet } from 'react-native';
+
+import { useController, useFormContext, ControllerProps, UseControllerProps } from 'react-hook-form';
+
+
+
+const ControlledInput = (props) => {
+  
+  const formContext = useFormContext();
+  const { formState } = formContext;
+
+  const {
+  name,
+  label,
+  defaultValue,
+  ...inputProps
+} = props;
+
+  const { field } = useController({ name, defaultValue });
+
+  const hasError = Boolean(formState?.errors[name]);
+
+  return (
+
+  <View style={styles.container}>
+    <View>
+      <RNTextInput
+        autoCapitalize="none"
+        textAlign="left"
+        style={styles.input}
+        onChangeText={field.onChange}
+        onBlur={field.onBlur}
+        value={field.value}
+        {...inputProps}
+      />
+
+      <View style={styles.errorContainer}>
+        {hasError && (<Text style={styles.error}>{formState.errors[name].message}</Text>)}
+      </View>
+
+    </View>
+  </View>
+
+  );
+}
+
+export const TextInput = (props) => {
+
+const {
+  name,
+  rules,
+  label,
+  defaultValue,
+  setFormError,
+  ...inputProps
+} = props;
+
+const formContext = useFormContext();
+
+// Placeholder until input name is initialized
+if (!formContext || !name) {
+  const msg = !formContext ? "TextInput must be wrapped by the FormProvider" : "Name must be defined"
+	console.error(msg)
+  setFormError(true)
+  return null
+}
+
+return <ControlledInput {...props} />;
+
+};
+
+
+const styles = StyleSheet.create({
+  label: {
+    margin: 20,
+    marginLeft: 0,
+  },
+  container: {
+    flex: -1,
+    justifyContent: 'center',
+    padding: 8,
+    borderColor: 'white',
+    borderWidth: 1
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+  errorContainer: {
+    flex: -1,
+    height: 25
+  },
+  error: {
+    color: 'red'
+  }
+});
