e14d12e46d897a6e3ab40395177eca3f11ba0281
Jeffery Miller <jeff.miller@safehealth.me>
2024-01-22 22:02:04 -0500

    JavaDoc Updates

--- opencdx-gateway/src/main/java/cdx/opencdx/gateway/package-info.java
+++ opencdx-gateway/src/main/java/cdx/opencdx/gateway/package-info.java
@@ -1,10 +1,10 @@
 /**
- * This is the main spring Application and supporting files
- * <p>
- * TODO: Insert Description
- * </p>
+ * The OpenCDX Gateway, built on the Spring Gateway, primarily routes REST and gRPC requests to the appropriate microservice for further processing.
  *
- * @author Safe Health LLC.
+ * @implSpec Gateway's gRPC routes require manual registration via modifications in the application.yml file.
+ * @implNote One distinctive feature of this class is that it prevents external calls to actuator endpoints.
+ *
+ * @author Safe Health LLC
  * @version 1.0
  * @since 1.0
  */
