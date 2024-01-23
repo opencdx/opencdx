ff08db6363d14b1705f04a5c03381c8e722690a8
Jeffery Miller <jeff.miller@safehealth.me>
2024-01-22 22:46:18 -0500

    JavaDoc

--- opencdx-helloworld/src/main/java/cdx/opencdx/helloworld/package-info.java
+++ opencdx-helloworld/src/main/java/cdx/opencdx/helloworld/package-info.java
@@ -1,8 +1,7 @@
 /**
- * This is the main spring Application and supporting files
- * <p>
- * TODO: Insert Description
- * </p>
+ * This code represents an exemplar microservice that serves as a blueprint for bootstrapping new microservices in
+ * OpenCDX. Begin by copying this microservice's structure, thereafter, execute a search
+ * for 'opencdx-helloworld' to pinpoint all the parts that need to be customized for your new microservice.
  *
  * @author Safe Health LLC.
  * @version 1.0
