74f07a33465c2ff7eab1d6364554a574b4ac75c2
Karthick Rajamurugan <karthick.rajamurugan@safehealth.me>
2024-02-15 19:35:34 +0530

    Final version of audit log table

--- opencdx-dashboard/src/pages/AuditLog.js
+++ opencdx-dashboard/src/pages/AuditLog.js
@@ -1,26 +1,50 @@
 import MainCard from 'ui-component/cards/MainCard';
 import Box from '@mui/material/Box';
 import DataGrid from 'ui-component/extended/DataGrid';
-
-import {  useEffect, useCallback } from 'react';
+import {  useEffect, useCallback, useState } from 'react';
 import axios from 'axios';
 
-
 export default function AuditLog() {
+  const [auditEventList, setAuditEventList] = useState([]);
     const fetchData = useCallback(async () => {
         try {
             const response = await axios.post('http://localhost:8632/graphql', {
                 query: `{
                     getAudit {
-                        purposeOfUse
+                        purposeOfUse,
+                        created,
+                        eventType,
+                        actor {
+                            identity_,
+                            role_,
+                            networkAddress_,
+                            agentType_
+                        },
+                        dataObject {
+                            resource_,
+                            data_,
+                            sensitivity_
+                        },
+                        auditSource {
+                            systemInfo_,
+                            configuration_
+                        },
+                        auditEntity {
+                            patientIdentifier_,
+                            userIdentifier_
+                        },
+                        _class,
+                        creator,
+                        modifier,
+                        modified
                     }
                 }`
             });
-
-            response.data.data?.getAudit.forEach((item) => {
-               console.log(item)
-            });
-
+          response.data.data?.getAudit.forEach((item, index) => {
+            item.id = index;
+          });
+          const modifiedAuditEventList = response.data.data?.getAudit;
+            setAuditEventList(modifiedAuditEventList);
         } catch (error) {
             console.error(error);
         }
@@ -31,8 +55,8 @@ export default function AuditLog() {
     }, [fetchData]);
     return (
         <MainCard title="Audit Log">
-            <Box sx={{ height: 400, width: '100%' }}>
-                <DataGrid />
+            <Box sx={{ height: 600, width: '100%' }}>
+                <DataGrid auditEventList={auditEventList} />
             </Box>
         </MainCard>
     );
