175838397d3e3d96c17f81972c294eea92dea743
Jeffery Miller <jeff.miller@safehealth.me>
2024-02-15 08:45:45 -0500

    fixed names

--- opencdx-client/src/test/java/cdx/opencdx/client/service/impl/OpenCDXTinkarClientImplTest.java
+++ opencdx-client/src/test/java/cdx/opencdx/client/service/impl/OpenCDXTinkarClientImplTest.java
@@ -55,7 +55,7 @@ class OpenCDXTinkarClientImplTest {
     }
 
     @Test
-    void sayTinkar() {
+    void searchTinkar() {
         Mockito.when(this.tinkarBlockingStub.searchTinkar(Mockito.any(TinkarQueryRequest.class)))
                 .thenReturn(TinkarQueryResponse.getDefaultInstance());
         OpenCDXCallCredentials openCDXCallCredentials = new OpenCDXCallCredentials("Bearer");
@@ -65,7 +65,7 @@ class OpenCDXTinkarClientImplTest {
     }
 
     @Test
-    void sayTinkarException() {
+    void searchTinkarException() {
         Mockito.when(this.tinkarBlockingStub.searchTinkar(Mockito.any(TinkarQueryRequest.class)))
                 .thenThrow(new StatusRuntimeException(Status.INTERNAL));
         TinkarQueryRequest request = TinkarQueryRequest.getDefaultInstance();
