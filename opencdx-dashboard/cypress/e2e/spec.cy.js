078ea6569570e9644eb0b0c64f09f925a2fb1a70
Karthick Rajamurugan <karthick.rajamurugan@safehealth.me>
2024-02-22 21:37:38 +0530

    Update spec for FormBuilder

--- opencdx-dashboard/cypress/e2e/spec.cy.js
+++ opencdx-dashboard/cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/form-builder')
+    cy.get('input[type=file]').selectFile('cypress/downloads/alpha.json',{force: true,action: 'drag-drop'})
+    cy.get('#user-form-json').click()
+    cy.get('#close-dialog').click()
+    cy.get('#anf-statement-json').click()
+    cy.get('#close-dialog').click()
+    cy.get('input[type=checkbox]').click()
   })
 })
