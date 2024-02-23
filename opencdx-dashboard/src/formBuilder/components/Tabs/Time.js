a4a18a2e80efc7d4bbd87abb44a1a7b62b101a3f
Karthick Rajamurugan <karthick.rajamurugan@safehealth.me>
2024-02-23 21:46:02 +0530

    Functional Component for SystemVariables & Styling fixes

--- opencdx-dashboard/src/formBuilder/components/Tabs/Time.js
+++ opencdx-dashboard/src/formBuilder/components/Tabs/Time.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { MeasureComponent } from '../TabComponents/MeasureComponent';
-import { Button, Grid } from '@mui/material';
-import { Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
-import SettingsIcon from '@mui/icons-material/Settings';
-
-import QuestionsList from '../ui-components/QuestionsList';
+import { SystemVariables } from '../ui-components/SystemVariables';
 
 const Time = React.forwardRef(({ control, register, index, currentIndex, getValues }, ref) => {
-    const [open, setOpen] = React.useState(false);
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-    const handleCloseDialog = () => {
-        setOpen(false);
-    };
     return (
         <>
-            <Grid container>
-                <Grid item xs={12} lg={12} ref={ref} justifyContent="flex-end" sx={{ display: 'flex', pb: 2 }}>
-                    <Button variant="contained" color="primary" size="small" onClick={handleClickOpen}>
-                        <SettingsIcon /> System Variables
-                    </Button>
-                </Grid>
-            </Grid>
-            <Dialog open={open} onClose={handleCloseDialog} fullWidth>
-                <DialogTitle>System Variables</DialogTitle>
-                <DialogContent>
-                    <QuestionsList index={index} currentIndex={currentIndex} getValues={getValues} tab="time" />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseDialog}>Close</Button>
-                </DialogActions>
-            </Dialog>
+           <SystemVariables index={index} currentIndex={currentIndex} getValues={getValues} tab="time" />
             <MeasureComponent {...{ control, register, index, currentIndex }} tab="time" ref={ref} />
         </>
     );
