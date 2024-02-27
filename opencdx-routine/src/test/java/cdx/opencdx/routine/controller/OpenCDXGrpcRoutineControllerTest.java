7f0169706fb51f63aa8488b6559bdf4e9ad75d5a
Jeffery Miller <jeff.miller@safehealth.me>
2024-02-27 09:12:52 -0500

    Replace DeliveryId with TrackingId in tests and controllers
    Updated multiple test cases and controllers to replace 'DeliveryId' with 'TrackingId'. The changes include test cases for OpenCDXRestRoutineController, OpenCDXGrpcRoutineController and OpenCDXRoutineServiceImpl. These revisions were made to correctly reflect the updated business logic in the model and to ensure the coherence of the application.

--- opencdx-routine/src/test/java/cdx/opencdx/routine/controller/OpenCDXGrpcRoutineControllerTest.java
+++ opencdx-routine/src/test/java/cdx/opencdx/routine/controller/OpenCDXGrpcRoutineControllerTest.java
@@ -125,7 +125,7 @@ class OpenCDXGrpcRoutineControllerTest {
         this.openCDXGrpcRoutineController.createDeliveryTracking(
                 DeliveryTrackingRequest.newBuilder(DeliveryTrackingRequest.getDefaultInstance())
                         .setDeliveryTracking(DeliveryTracking.newBuilder()
-                                .setDeliveryId("789")
+                                .setTrackingId("789")
                                 .build())
                         .build(),
                 responseObserver);
@@ -139,7 +139,7 @@ class OpenCDXGrpcRoutineControllerTest {
         this.openCDXGrpcRoutineController.getDeliveryTracking(
                 DeliveryTrackingRequest.newBuilder()
                         .setDeliveryTracking(DeliveryTracking.newBuilder()
-                                .setDeliveryId("789")
+                                .setTrackingId("789")
                                 .build())
                         .build(),
                 responseObserver);
