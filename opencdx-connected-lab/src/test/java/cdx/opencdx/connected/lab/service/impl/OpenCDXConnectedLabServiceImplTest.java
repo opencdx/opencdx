9af65e6a0eb575fb18e0d2ba4c81c4a2e1477529
Jeffery Miller <jeff.miller@safehealth.me>
2024-03-07 09:03:46 -0500

    Add additional tests to OpenCDXConnectedLabServiceImplTest and amend OpenCDXShippingServiceImplTest
    Added specific testing scenarios to OpenCDXConnectedLabServiceImplTest class, particularly checking list sorting options, mock situations causing exceptions and absence of connected labs. In addition, a minor test name correction has been made in OpenCDXShippingServiceImplTest to reflect the test's functionality correctly. These enhancements improve test coverage and ensure the tested methods behave as expected under different conditions.

--- opencdx-connected-lab/src/test/java/cdx/opencdx/connected/lab/service/impl/OpenCDXConnectedLabServiceImplTest.java
+++ opencdx-connected-lab/src/test/java/cdx/opencdx/connected/lab/service/impl/OpenCDXConnectedLabServiceImplTest.java
@@ -17,7 +17,10 @@ package cdx.opencdx.connected.lab.service.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import cdx.opencdx.commons.exceptions.OpenCDXNotAcceptable;
+import cdx.opencdx.commons.exceptions.OpenCDXNotFound;
 import cdx.opencdx.commons.model.OpenCDXIAMUserModel;
+import cdx.opencdx.commons.repository.OpenCDXProfileRepository;
 import cdx.opencdx.commons.service.OpenCDXAuditService;
 import cdx.opencdx.commons.service.OpenCDXCurrentUser;
 import cdx.opencdx.connected.lab.config.OpenCDXLabConnectionFactoryBean;
@@ -27,9 +30,11 @@ import cdx.opencdx.connected.lab.service.OpenCDXConnectedLabService;
 import cdx.opencdx.grpc.common.Pagination;
 import cdx.opencdx.grpc.connected.BasicInfo;
 import cdx.opencdx.grpc.lab.connected.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.Timestamp;
 import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
@@ -222,5 +227,235 @@ class OpenCDXConnectedLabServiceImplTest {
 
         Assertions.assertDoesNotThrow(
                 () -> this.openCDXConnectedLabService.listConnectedLabs(listConnectedLabsRequest));
+    }
+    @Test
+    void listConnectedLabs_SortAsc() {
+        ListConnectedLabsRequest listConnectedLabsRequest = ListConnectedLabsRequest.newBuilder()
+                .setPagination(
+                        Pagination.newBuilder().setPageNumber(1).setPageSize(10).setSort("id").setSortAscending(true).build())
+                .build();
+
+        Assertions.assertDoesNotThrow(
+                () -> this.openCDXConnectedLabService.listConnectedLabs(listConnectedLabsRequest));
+    }
+    @Test
+    void listConnectedLabs_SortDesc() {
+        ListConnectedLabsRequest listConnectedLabsRequest = ListConnectedLabsRequest.newBuilder()
+                .setPagination(
+                        Pagination.newBuilder().setPageNumber(1).setPageSize(10).setSort("id").setSortAscending(false).build())
+                .build();
+
+        Assertions.assertDoesNotThrow(
+                () -> this.openCDXConnectedLabService.listConnectedLabs(listConnectedLabsRequest));
+    }
+    @Test
+    void listConnectedLabs_None() {
+        Mockito.when(this.openCDXConnectedLabRepository.findAll(Mockito.any(Pageable.class)))
+                .thenReturn(new PageImpl<>(Collections.emptyList(), PageRequest.of(1, 10), 1));
+
+        this.openCDXConnectedLabService = new OpenCDXConnectedLabServiceImpl(
+                openCDXCurrentUser,
+                openCDXAuditService,
+                objectMapper,
+                openCDXConnectedLabRepository,
+                openCDXLabConnectionFactoryBean);
+
+        ListConnectedLabsRequest listConnectedLabsRequest = ListConnectedLabsRequest.newBuilder()
+                .setPagination(
+                        Pagination.newBuilder().setPageNumber(1).setPageSize(10).build())
+                .build();
+
+        Assertions.assertDoesNotThrow(
+                () -> this.openCDXConnectedLabService.listConnectedLabs(listConnectedLabsRequest));
+    }
+
+    @Test
+    void submitLabFindings_OnlyOrganization() {
+        Mockito.when(this.openCDXConnectedLabRepository.findByOrganizationIdAndWorkspaceId(
+                Mockito.any(ObjectId.class), Mockito.any(ObjectId.class)))
+                .thenReturn(Optional.empty());
+        Mockito.when(this.openCDXConnectedLabRepository.findByOrganizationId(
+                        Mockito.any(ObjectId.class)))
+                .thenAnswer(new Answer<Optional<OpenCDXConnectedLabModel>>() {
+                    @Override
+                    public Optional<OpenCDXConnectedLabModel> answer(InvocationOnMock invocation) throws Throwable {
+                        ObjectId argument = invocation.getArgument(0);
+
+                        return Optional.of(OpenCDXConnectedLabModel.builder()
+                                .id(ObjectId.get())
+                                .identifier("default")
+                                .created(Instant.now())
+                                .modified(Instant.now())
+                                .creator(ObjectId.get())
+                                .modifier(ObjectId.get())
+                                .build());
+                    }
+                });
+        this.openCDXConnectedLabService = new OpenCDXConnectedLabServiceImpl(
+                openCDXCurrentUser,
+                openCDXAuditService,
+                objectMapper,
+                openCDXConnectedLabRepository,
+                openCDXLabConnectionFactoryBean);
+        LabFindings labFindings = LabFindings.newBuilder()
+                .setBasicInfo(BasicInfo.newBuilder()
+                        .setOrganizationId(ObjectId.get().toHexString())
+                        .setWorkspaceId(ObjectId.get().toHexString())
+                        .build())
+                .build();
+
+        Assertions.assertDoesNotThrow(() -> this.openCDXConnectedLabService.submitLabFindings(labFindings));
+    }
+
+    @Test
+    void createConnectedLab_OpenCDXNotAcceptable() throws JsonProcessingException {
+        ObjectMapper mapper = Mockito.mock(ObjectMapper.class);
+        Mockito.when(mapper.writeValueAsString(Mockito.any(OpenCDXConnectedLabModel.class)))
+                .thenThrow(JsonProcessingException.class);
+
+        this.openCDXConnectedLabService = new OpenCDXConnectedLabServiceImpl(
+                openCDXCurrentUser,
+                openCDXAuditService,
+                mapper,
+                openCDXConnectedLabRepository,
+                openCDXLabConnectionFactoryBean);
+
+        CreateConnectedLabRequest createConnectedLabRequest = CreateConnectedLabRequest.newBuilder()
+                .setConnectedLab(ConnectedLab.newBuilder()
+                        .setIdentifier("default")
+                        .setOrganizationId(ObjectId.get().toHexString())
+                        .setWorkspaceId(ObjectId.get().toHexString())
+                        .setCreated(Timestamp.newBuilder().setSeconds(1000000).build())
+                        .setModified(Timestamp.newBuilder().setSeconds(1000000).build())
+                        .setCreator(ObjectId.get().toHexString())
+                        .setModifier(ObjectId.get().toHexString())
+                        .build())
+                .build();
+
+        Assertions.assertThrows(
+                OpenCDXNotAcceptable.class,() -> this.openCDXConnectedLabService.createConnectedLab(createConnectedLabRequest));
+    }
+
+    @Test
+    void updateConnectedLab_OpenCDXNotAcceptable() throws JsonProcessingException {
+        ObjectMapper mapper = Mockito.mock(ObjectMapper.class);
+        Mockito.when(mapper.writeValueAsString(Mockito.any(OpenCDXConnectedLabModel.class)))
+                .thenThrow(JsonProcessingException.class);
+
+        this.openCDXConnectedLabService = new OpenCDXConnectedLabServiceImpl(
+                openCDXCurrentUser,
+                openCDXAuditService,
+                mapper,
+                openCDXConnectedLabRepository,
+                openCDXLabConnectionFactoryBean);
+
+        UpdateConnectedLabRequest createConnectedLabRequest = UpdateConnectedLabRequest.newBuilder()
+                .setConnectedLab(ConnectedLab.newBuilder()
+                        .setId(ObjectId.get().toHexString())
+                        .setIdentifier("default")
+                        .setOrganizationId(ObjectId.get().toHexString())
+                        .setWorkspaceId(ObjectId.get().toHexString())
+                        .setCreated(Timestamp.newBuilder().setSeconds(1000000).build())
+                        .setModified(Timestamp.newBuilder().setSeconds(1000000).build())
+                        .setCreator(ObjectId.get().toHexString())
+                        .setModifier(ObjectId.get().toHexString())
+                        .build())
+                .build();
+
+        Assertions.assertThrows(
+                OpenCDXNotAcceptable.class,() -> this.openCDXConnectedLabService.updateConnectedLab(createConnectedLabRequest));
+    }
+
+    @Test
+    void deleteConnectedLab_OpenCDXNotAcceptable() throws JsonProcessingException {
+        ObjectMapper mapper = Mockito.mock(ObjectMapper.class);
+        Mockito.when(mapper.writeValueAsString(Mockito.any(OpenCDXConnectedLabModel.class)))
+                .thenThrow(JsonProcessingException.class);
+
+        this.openCDXConnectedLabService = new OpenCDXConnectedLabServiceImpl(
+                openCDXCurrentUser,
+                openCDXAuditService,
+                mapper,
+                openCDXConnectedLabRepository,
+                openCDXLabConnectionFactoryBean);
+
+        DeleteConnectedLabRequest deleteConnectedLabRequest = DeleteConnectedLabRequest.newBuilder()
+                .setConnectedLabId(ObjectId.get().toHexString())
+                .build();
+
+        Assertions.assertThrows(
+                OpenCDXNotAcceptable.class,() -> this.openCDXConnectedLabService.deleteConnectedLab(deleteConnectedLabRequest));
+    }
+
+    @Test
+    void getConnectedLab_OpenCDXNotFound() throws JsonProcessingException {
+        this.openCDXConnectedLabRepository = Mockito.mock(OpenCDXConnectedLabRepository.class);
+        Mockito.when(this.openCDXConnectedLabRepository.findById(Mockito.any(ObjectId.class)))
+                .thenReturn(Optional.empty());
+
+        this.openCDXConnectedLabService = new OpenCDXConnectedLabServiceImpl(
+                openCDXCurrentUser,
+                openCDXAuditService,
+                objectMapper,
+                openCDXConnectedLabRepository,
+                openCDXLabConnectionFactoryBean);
+
+        GetConnectedLabRequest getConnectedLabRequest = GetConnectedLabRequest.newBuilder()
+                .setConnectedLabId(ObjectId.get().toHexString())
+                .build();
+
+        Assertions.assertThrows(
+                OpenCDXNotFound.class,() -> this.openCDXConnectedLabService.getConnectedLab(getConnectedLabRequest));
+    }
+
+    @Test
+    void updateConnectedLab_OpenCDXNotFound() throws JsonProcessingException {
+        this.openCDXConnectedLabRepository = Mockito.mock(OpenCDXConnectedLabRepository.class);
+        Mockito.when(this.openCDXConnectedLabRepository.findById(Mockito.any(ObjectId.class)))
+                .thenReturn(Optional.empty());
+
+        this.openCDXConnectedLabService = new OpenCDXConnectedLabServiceImpl(
+                openCDXCurrentUser,
+                openCDXAuditService,
+                objectMapper,
+                openCDXConnectedLabRepository,
+                openCDXLabConnectionFactoryBean);
+
+        UpdateConnectedLabRequest createConnectedLabRequest = UpdateConnectedLabRequest.newBuilder()
+                .setConnectedLab(ConnectedLab.newBuilder()
+                        .setId(ObjectId.get().toHexString())
+                        .setIdentifier("default")
+                        .setOrganizationId(ObjectId.get().toHexString())
+                        .setWorkspaceId(ObjectId.get().toHexString())
+                        .setCreated(Timestamp.newBuilder().setSeconds(1000000).build())
+                        .setModified(Timestamp.newBuilder().setSeconds(1000000).build())
+                        .setCreator(ObjectId.get().toHexString())
+                        .setModifier(ObjectId.get().toHexString())
+                        .build())
+                .build();
+
+        Assertions.assertThrows(
+                OpenCDXNotFound.class,() -> this.openCDXConnectedLabService.updateConnectedLab(createConnectedLabRequest));
+    }
+
+    @Test
+    void deleteConnectedLab_OpenCDXNotFound() throws JsonProcessingException {
+        this.openCDXConnectedLabRepository = Mockito.mock(OpenCDXConnectedLabRepository.class);
+        Mockito.when(this.openCDXConnectedLabRepository.findById(Mockito.any(ObjectId.class)))
+                .thenReturn(Optional.empty());
+
+        this.openCDXConnectedLabService = new OpenCDXConnectedLabServiceImpl(
+                openCDXCurrentUser,
+                openCDXAuditService,
+                objectMapper,
+                openCDXConnectedLabRepository,
+                openCDXLabConnectionFactoryBean);
+
+        DeleteConnectedLabRequest deleteConnectedLabRequest = DeleteConnectedLabRequest.newBuilder()
+                .setConnectedLabId(ObjectId.get().toHexString())
+                .build();
+
+        Assertions.assertThrows(
+                OpenCDXNotFound.class,() -> this.openCDXConnectedLabService.deleteConnectedLab(deleteConnectedLabRequest));
     }
 }
