b9ef6b6b684728d7afbd3fd68556065351c6e1a8
Jeffery Miller <jeff.miller@safehealth.me>
2024-03-26 16:10:21 -0400

    Refactor method name from 'setResult' to 'setFurtherActions'
    Changed the method name from 'setResult' to 'setFurtherActions' in the BloodPressureRules class. The new method name better represents its intended function by explicitly indicating that it sets the follow-up actions based on the diagnosed blood pressure level.

--- opencdx-classification-analyzer/src/main/java/cdx/opencdx/classification/analyzer/rules/BloodPressureRules.java
+++ opencdx-classification-analyzer/src/main/java/cdx/opencdx/classification/analyzer/rules/BloodPressureRules.java
@@ -1,5 +1,4 @@
 package cdx.opencdx.classification.analyzer.rules;
-
 import cdx.opencdx.classification.analyzer.dto.RuleResult;
 import org.evrete.dsl.annotation.Fact;
 import org.evrete.dsl.annotation.Rule;
@@ -10,16 +9,16 @@ public class BloodPressureRules {
     @Rule
     @Where("$s < 120")
     public void normalBloodPressure(@Fact("$s") int systolic, RuleResult ruleResult) {
-        ruleResult.setResult("Normal blood pressure. No further actions needed.");
+        ruleResult.setFurtherActions("Normal blood pressure. No further actions needed.");
     }
     @Rule
     @Where("$s >= 120 && $s <= 129")
     public void elevatedBloodPressure(@Fact("$s") int systolic, RuleResult ruleResult) {
-        ruleResult.setResult("Elevated blood pressure. Please continue monitoring.");
+        ruleResult.setFurtherActions("Elevated blood pressure. Please continue monitoring.");
     }
     @Rule
     @Where("$s > 129")
     public void highBloodPressure(@Fact("$s") int systolic, RuleResult ruleResult) {
-        ruleResult.setResult("High blood pressure. Please seek additional assistance.");
+        ruleResult.setFurtherActions("High blood pressure. Please seek additional assistance.");
     }
 }
