32207b678c45a6b788c06be46750a3740c624ccf
Jeffery Miller <jeff.miller@safehealth.me>
2024-03-26 13:44:38 -0400

    Add opencdx-classification-analyzer module to opencdx project
    A new opencdx-classification-analyzer module is added to the existing opencdx project. This module includes configuration files, gradle build scripts, test environments settings, and common library. README and gitignore files are also provided for the new module. The necessary dependencies are defined in build.gradle file. Finally, the new module is set up for Spring Boot autoconfiguration.

---
+++ opencdx-classification-analyzer/src/main/java/cdx/opencdx/classification/analyzer/config/AnalyzerConfig.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2024 Safe Health Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cdx.opencdx.classification.analyzer.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Autoconfiguration class for opencdx-classification-analyzer.
+ */
+@Slf4j
+@AutoConfiguration
+@Configuration
+public class AnalyzerConfig {
+    /**
+     * Default Constructor
+     */
+    public AnalyzerConfig() {
+        // Explicit declaration to prevent this class from inadvertently being made instantiable
+        log.info("Classification Analyzer Configuration Initializing.");
+    }
+
+
+}
