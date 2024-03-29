bec020d3b9ce36df2966c00850e2234b63dcfe9e
Jeffery Miller <jeff.miller@safehealth.me>
2024-03-28 23:40:18 -0400

    Integrate KnowledgeService as class variable in `OpenCDXAnalysisEngineImpl`
    This commit adds KnowledgeService as a class variable in `OpenCDXAnalysisEngineImpl`. This tweak not only makes the class easier to read but can also enhance performance by preventing regular instantiation of KnowledgeService in the method body each time it's called. Instead, a single instance is used throughout the class life cycle, improving efficiency.

--- opencdx-classification-analyzer/src/main/java/cdx/opencdx/classification/analyzer/service/impl/OpenCDXAnalysisEngineImpl.java
+++ opencdx-classification-analyzer/src/main/java/cdx/opencdx/classification/analyzer/service/impl/OpenCDXAnalysisEngineImpl.java
@@ -62,6 +62,7 @@ public class OpenCDXAnalysisEngineImpl implements OpenCDXAnalysisEngine {
     private final OpenCDXMediaUpDownClient openCDXMediaUpDownClient;
     private final OpenCDXTestCaseClient openCDXTestCaseClient;
     private final OpenCDXCurrentUser openCDXCurrentUser;
+    private final KnowledgeService knowledgeService;
     /**
      * Random number generator for demonstration purposes to generate random classification responses.
      */
@@ -84,6 +85,7 @@ public class OpenCDXAnalysisEngineImpl implements OpenCDXAnalysisEngine {
         this.openCDXTestCaseClient = openCDXTestCaseClient;
         this.openCDXCurrentUser = openCDXCurrentUser;
         this.random = new Random();
+        this.knowledgeService = new KnowledgeService();
     }
 
     /**
@@ -230,7 +232,6 @@ public class OpenCDXAnalysisEngineImpl implements OpenCDXAnalysisEngine {
                 && !userQuestionnaireData.getQuestionnaireData(0).getRuleId().isBlank()
                 && userQuestionnaireData.getQuestionnaireData(0).getRuleQuestionIdCount() > 0) {
 
-            KnowledgeService knowledgeService = new KnowledgeService();
             Knowledge knowledge = null;
 
             try {
