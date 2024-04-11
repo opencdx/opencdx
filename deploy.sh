d8a0c331bed8b5cbd352e2e1baafbc093358aeb9
Jeffery Miller <jeff@mymiller.name>
2024-04-11 15:35:07 -0400

    Remove 'opencdx-connected-lab' components and related configurations
    This commit removes all 'opencdx-connected-lab' components from the project. Includes removal of services, interfaces, configurations, tests, and other related resources. This component was removed across multiple scripts and build files to maintain consistency across the project.

--- deploy.sh
+++ deploy.sh
@@ -377,8 +377,7 @@ build_docker() {
   local auto_confirm_all=$2
 
   components=("opencdx/mongodb" "opencdx/admin" "opencdx/config" "opencdx/tinkar"
-    "opencdx/audit" "opencdx/communications" "opencdx/media" "opencdx/health" "opencdx/connected-lab"
-    "opencdx/iam"
+    "opencdx/audit" "opencdx/communications" "opencdx/media" "opencdx/health" "opencdx/iam"
     "opencdx/questionnaire" "opencdx/classification" "opencdx/gateway" "opencdx/logistics"
     "opencdx/discovery" "opencdx/anf" "opencdx/graphql")
 
@@ -901,7 +900,7 @@ if [ "$no_menu" = false ]; then
         open_reports "admin";
         if [ "$jmeter" = true ]; then
             handle_info "Waiting to run $jmeter_test tests"
-            countdown 300
+            countdown 150
             run_jmeter_tests $jmeter_test
             open_url "build/reports/jmeter/index.html"
         fi
