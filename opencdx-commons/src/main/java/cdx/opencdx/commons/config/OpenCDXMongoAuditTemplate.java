3e81ebc5b1f869636bcfef246b4f2cdc114b713f
Jeffery Miller <jeff@mymiller.name>
2024-05-03 11:48:51 -0400

    Refactor import statements and remove unused MongoDB operations
    This commit refactors the import statements, reorganizing them for clarity in both the OpenCDXAuditServiceImpl and OpenCDXMongoAuditTemplate classes. It also removes several unused methods related to MongoDB operations in OpenCDXMongoAuditTemplate class, simplifying the code and making it easier to maintain.

--- opencdx-commons/src/main/java/cdx/opencdx/commons/config/OpenCDXMongoAuditTemplate.java
+++ opencdx-commons/src/main/java/cdx/opencdx/commons/config/OpenCDXMongoAuditTemplate.java
@@ -17,20 +17,14 @@ package cdx.opencdx.commons.config;
 
 import cdx.opencdx.commons.annotations.ExcludeFromJacocoGeneratedReport;
 import cdx.opencdx.commons.data.OpenCDXIdentifier;
-import cdx.opencdx.commons.utils.CurrentUserHelper;
-import com.mongodb.client.result.DeleteResult;
 import io.micrometer.observation.annotation.Observed;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.time.Instant;
-import java.util.List;
-import lombok.extern.slf4j.Slf4j;
-import org.bson.Document;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.*;
-import org.springframework.data.mongodb.core.convert.MongoConverter;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 
 /**
  * OpenCDX MongoAudit to ensure Creator/Created and Modifier/Modified are set on each and every
@@ -73,63 +67,6 @@ public class OpenCDXMongoAuditTemplate extends MongoTemplate {
         }
 
         return super.maybeCallBeforeConvert(object, collection);
-    }
-
-    @Override
-    protected <T> T maybeCallBeforeSave(T object, Document document, String collection) {
-        log.trace("OpenCDXMongoAuditTemplate maybeCallBeforeSave");
-        OpenCDXIdentifier identityID =
-                CurrentUserHelper.getOpenCDXCurrentUser().getCurrentUser().getId();
-        Instant date = Instant.now();
-
-        Object id = document.get("_id");
-        Object creator = document.get("creator");
-        if (id == null || creator == null) {
-            log.debug("Setting Creator: {} created: {}", identityID, date);
-            document.put("creator", identityID);
-            document.put("created", date);
-        }
-        log.debug("Setting Modifier: {} modified: {}", identityID, date);
-        document.put("modifier", identityID);
-        document.put("modified", date);
-
-        return super.maybeCallBeforeSave(object, document, collection);
-    }
-
-    @Override
-    public <T> T findAndRemove(Query query, Class<T> entityClass, String collectionName) {
-        log.trace("OpenCDXMongoAuditTemplate findAndRemove");
-        updateRemovalQuery(collectionName, query);
-        return super.findAndRemove(query, entityClass, collectionName);
-    }
-
-    @Override
-    public <T> List<T> findAllAndRemove(Query query, Class<T> entityClass, String collectionName) {
-        log.trace("OpenCDXMongoAuditTemplate findAllAndRemove");
-        updateRemovalQuery(collectionName, query);
-        return super.findAllAndRemove(query, entityClass, collectionName);
-    }
-
-    @Override
-    protected <T> DeleteResult doRemove(String collectionName, Query query, Class<T> entityClass, boolean multi) {
-        log.trace("OpenCDXMongoAuditTemplate doRemove");
-        updateRemovalQuery(collectionName, query);
-        return super.doRemove(collectionName, query, entityClass, multi);
-    }
-
-    private void updateRemovalQuery(String collectionName, Query query) {
-        log.trace("OpenCDXMongoAuditTemplate updateRemovalQuery");
-        OpenCDXIdentifier identityID =
-                CurrentUserHelper.getOpenCDXCurrentUser().getCurrentUser().getId();
-        Instant date = Instant.now();
-
-        log.debug("Setting Modifier: {} modified: {}", identityID, date);
-
-        Update update = new Update();
-        update.set("modifier", identityID);
-        update.set("modified", date);
-
-        this.findAndModify(query, update, Document.class, collectionName);
     }
 
     // Method to check if a class has a specific method
