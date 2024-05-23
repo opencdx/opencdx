01e57314a09e5e7f2fb645f26ce5d97b5c086bd9
santoshjami <santosh.jami@hotmail.com>
2024-05-23 13:09:45 -0400

    Interface, Service, Repository implementation for MedicalHistory

---
+++ opencdx-health/src/main/java/cdx/opencdx/health/controller/OpenCDXMedicalHistoryGrpcController.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2024 Safe Health Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cdx.opencdx.health.controller;
+
+import cdx.opencdx.grpc.data.MedicalHistory;
+import cdx.opencdx.grpc.service.health.MedicalHistoryServiceGrpc;
+
+import cdx.opencdx.grpc.service.health.CreateMedicalHistoryRequest;
+import cdx.opencdx.grpc.service.health.CreateMedicalHistoryResponse;
+import cdx.opencdx.grpc.service.health.GetMedicalHistoryRequest;
+import cdx.opencdx.grpc.service.health.GetMedicalHistoryResponse;
+import cdx.opencdx.grpc.service.health.UpdateMedicalHistoryRequest;
+import cdx.opencdx.grpc.service.health.UpdateMedicalHistoryResponse;
+import cdx.opencdx.grpc.service.health.DeleteMedicalHistoryRequest;
+import cdx.opencdx.grpc.service.health.SuccessResponse;
+import cdx.opencdx.grpc.service.health.ListMedicalHistoriesRequest;
+import cdx.opencdx.grpc.service.health.ListMedicalHistoriesResponse;
+
+import cdx.opencdx.health.service.OpenCDXMedicalHistoryService;
+
+import io.grpc.stub.StreamObserver;
+import io.micrometer.observation.annotation.Observed;
+import lombok.extern.slf4j.Slf4j;
+import org.lognet.springboot.grpc.GRpcService;
+
+/**
+ * gRPC Controller MedicalHistory service
+ */
+@Slf4j
+@GRpcService
+@Observed(name = "opencdx")
+public class OpenCDXMedicalHistoryGrpcController extends MedicalHistoryServiceGrpc.MedicalHistoryServiceImplBase {
+    private final OpenCDXMedicalHistoryService openCDXMedicalHistoryService;
+
+    /**
+     * Constructor that takes a OpenCDXMedicalHistoryService
+     * @param openCDXMedicalHistoryService service for procesing requests
+     */
+    public OpenCDXMedicalHistoryGrpcController(OpenCDXMedicalHistoryService openCDXMedicalHistoryService) {
+        this.openCDXMedicalHistoryService = openCDXMedicalHistoryService;
+    }
+
+    /**
+     * Method to track when MedicalHistory is given to a patient.
+     * @param request Request for the MedicalHistory administration.
+     * @param responseObserver The response observer.
+     */
+    @Override
+    public void createMedicalHistory(CreateMedicalHistoryRequest request, StreamObserver<CreateMedicalHistoryResponse> responseObserver) {
+        responseObserver.onNext(this.openCDXMedicalHistoryService.createMedicalHistory(request));
+        responseObserver.onCompleted();
+    }
+    /**
+     * Method to get MedicalHistory information by ID.
+     * @param request Request for the MedicalHistory by ID.
+     * @param responseObserver The response observer.
+     */
+    @Override
+    public void getMedicalHistory(GetMedicalHistoryRequest request, StreamObserver<GetMedicalHistoryResponse> responseObserver) {
+        responseObserver.onNext(this.openCDXMedicalHistoryService.getMedicalHistory(request));
+        responseObserver.onCompleted();
+    }
+
+    /**
+     * Method to get MedicalHistory information by ID.
+     * @param request Request for the MedicalHistory by ID.
+     * @param responseObserver The response observer.
+     */
+    @Override
+    public void updateMedicalHistory(UpdateMedicalHistoryRequest request, StreamObserver<UpdateMedicalHistoryResponse> responseObserver) {
+        responseObserver.onNext(this.openCDXMedicalHistoryService.updateMedicalHistory(request));
+        responseObserver.onCompleted();
+    }
+
+    /**
+     * Method to get MedicalHistory information by ID.
+     * @param request Request for the MedicalHistory by ID.
+     * @param responseObserver The response observer.
+     */
+    @Override
+    public void deleteMedicalHistory(DeleteMedicalHistoryRequest request, StreamObserver<SuccessResponse> responseObserver) {
+        responseObserver.onNext(this.openCDXMedicalHistoryService.deleteMedicalHistory(request));
+        responseObserver.onCompleted();
+    }
+    /**
+     * Method to get MedicalHistory information by patient ID within a date range.
+     * @param request Request for the MedicalHistory by patient ID
+     * @param responseObserver The response observer.
+     */
+    @Override
+    public void listMedicalHistories(ListMedicalHistoriesRequest request, StreamObserver<ListMedicalHistoriesResponse> responseObserver) {
+        responseObserver.onNext(this.openCDXMedicalHistoryService.listMedicalHistories(request));
+        responseObserver.onCompleted();
+    }
+}
---
+++ opencdx-health/src/test/java/cdx/opencdx/health/service/impl/OpenCDXMedicalHistoryServiceImplTest.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright 2024 Safe Health Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cdx.opencdx.health.service.impl;
+
+import static org.mockito.ArgumentMatchers.any;
+
+import cdx.opencdx.commons.data.OpenCDXIdentifier;
+import cdx.opencdx.commons.exceptions.OpenCDXNotAcceptable;
+import cdx.opencdx.commons.exceptions.OpenCDXNotFound;
+import cdx.opencdx.commons.model.OpenCDXIAMUserModel;
+import cdx.opencdx.commons.service.OpenCDXAuditService;
+import cdx.opencdx.commons.service.OpenCDXCurrentUser;
+import cdx.opencdx.commons.service.OpenCDXDocumentValidator;
+import cdx.opencdx.grpc.data.Pagination;
+import cdx.opencdx.grpc.data.MedicalHistory;
+import cdx.opencdx.grpc.service.health.CreateMedicalHistoryRequest;
+import cdx.opencdx.grpc.service.health.GetMedicalHistoryRequest;
+import cdx.opencdx.grpc.service.health.UpdateMedicalHistoryRequest;
+import cdx.opencdx.grpc.service.health.DeleteMedicalHistoryRequest;
+import cdx.opencdx.grpc.service.health.SuccessResponse;
+import cdx.opencdx.grpc.service.health.ListMedicalHistoriesRequest;
+
+import cdx.opencdx.health.model.OpenCDXMedicalHistoryModel;
+import cdx.opencdx.health.repository.OpenCDXMedicalHistoryRepository;
+import cdx.opencdx.health.service.OpenCDXMedicalHistoryService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ActiveProfiles({"test", "managed"})
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(properties = {"spring.cloud.config.enabled=false", "mongock.enabled=false"})
+class OpenCDXMedicalHistoryServiceImplTest {
+
+
+    OpenCDXMedicalHistoryService openCDXMedicalHistoryService;
+
+    @Autowired
+    OpenCDXAuditService openCDXAuditService;
+
+    @Autowired
+    OpenCDXDocumentValidator openCDXDocumentValidator;
+
+    @Mock
+    ObjectMapper objectMapper;
+
+    @Mock
+    OpenCDXCurrentUser openCDXCurrentUser;
+
+    @Mock
+    OpenCDXMedicalHistoryRepository openCDXMedicalHistoryRepository;
+
+    @BeforeEach
+    void setUp() throws JsonProcessingException {
+        Mockito.when(this.openCDXCurrentUser.getCurrentUser())
+                .thenReturn(OpenCDXIAMUserModel.builder()
+                        .id(OpenCDXIdentifier.get())
+                        .build());
+        Mockito.when(this.openCDXCurrentUser.getCurrentUser(Mockito.any(OpenCDXIAMUserModel.class)))
+                .thenReturn(OpenCDXIAMUserModel.builder()
+                        .id(OpenCDXIdentifier.get())
+                        .build());
+
+        Mockito.when(this.openCDXMedicalHistoryRepository.save(Mockito.any(OpenCDXMedicalHistoryModel.class)))
+                .thenAnswer(new Answer<OpenCDXVaccineModel>() {
+                    @Override
+                    public OpenCDXVaccineModel answer(InvocationOnMock invocation) throws Throwable {
+                        OpenCDXMedicalHistoryModel argument = invocation.getArgument(0);
+                        if (argument.getId() == null) {
+                            argument.setId(OpenCDXIdentifier.get());
+                        }
+                        return argument;
+                    }
+                });
+
+        Mockito.when(this.openCDXMedicalHistoryRepository.findById(Mockito.any(OpenCDXIdentifier.class)))
+                .thenAnswer(new Answer<Optional<OpenCDXVaccineModel>>() {
+                    @Override
+                    public Optional<OpenCDXVaccineModel> answer(InvocationOnMock invocation) throws Throwable {
+                        OpenCDXIdentifier argument = invocation.getArgument(0);
+                        return Optional.of(OpenCDXMedicalHistoryModel.builder()
+                                .id(OpenCDXIdentifier.get())
+                                .patientId(OpenCDXIdentifier.get())
+                                .nationalHealthId(UUID.randomUUID().toString())
+                                .build());
+                    }
+                });
+
+        Mockito.when(this.openCDXMedicalHistoryRepository.findAllByPatientId(
+                        Mockito.any(OpenCDXIdentifier.class), Mockito.any(Pageable.class)))
+                .thenReturn(new PageImpl<>(
+                        List.of(OpenCDXMedicalHistoryModel.builder()
+                                .id(OpenCDXIdentifier.get())
+                                .patientId(OpenCDXIdentifier.get())
+                                .nationalHealthId(UUID.randomUUID().toString())
+                                .build()),
+                        PageRequest.of(1, 10),
+                        1));
+
+        Mockito.when(this.openCDXMedicalHistoryRepository.findAllByNationalHealthId(
+                        Mockito.any(String.class), Mockito.any(Pageable.class)))
+                .thenReturn(new PageImpl<>(
+                        List.of(OpenCDXMedicalHistoryModel.builder()
+                                .id(OpenCDXIdentifier.get())
+                                .patientId(OpenCDXIdentifier.get())
+                                .nationalHealthId(UUID.randomUUID().toString())
+                                .build()),
+                        PageRequest.of(1, 10),
+                        1));
+
+        this.objectMapper = Mockito.mock(ObjectMapper.class);
+        Mockito.when(this.objectMapper.writeValueAsString(any())).thenThrow(JsonProcessingException.class);
+
+        this.openCDXMedicalHistoryService = new OpenCDXMedicalHistoryServiceImpl(
+                this.objectMapper,
+                this.openCDXAuditService,
+                this.openCDXCurrentUser,
+                this.openCDXMedicalHistoryRepository,
+                this.openCDXDocumentValidator);
+    }
+
+    @Test
+    void createMedicalHistory() {
+        Vaccine vaccine = Vaccine.newBuilder()
+                .setId(ObjectId.get().toHexString())
+                .setPatientId(ObjectId.get().toHexString())
+                .setNationalHealthId(ObjectId.get().toHexString())
+                .setFips("fips")
+                .setHealthDistrict("district")
+                .setDoseNumber(2)
+                .build();
+        Assertions.assertThrows(
+                OpenCDXNotAcceptable.class, () -> openCDXVaccineService.trackVaccineAdministration(vaccine));
+    }
+
+    @Test
+    void getVaccineById() {
+        GetVaccineByIdRequest request = GetVaccineByIdRequest.newBuilder()
+                .setId(ObjectId.get().toHexString())
+                .build();
+        Assertions.assertThrows(OpenCDXNotAcceptable.class, () -> openCDXVaccineService.getVaccineById(request));
+    }
+
+    @Test
+    void listVaccinesSortNotAscending() {
+        ListVaccinesRequest request = ListVaccinesRequest.newBuilder()
+                .setPagination(Pagination.newBuilder()
+                        .setPageNumber(1)
+                        .setPageSize(10)
+                        .setSortAscending(false)
+                        .setSort("id")
+                        .build())
+                .setNationalHealthId(OpenCDXIdentifier.get().toHexString())
+                .build();
+        Assertions.assertThrows(OpenCDXNotAcceptable.class, () -> openCDXVaccineService.listVaccines(request));
+    }
+
+    @Test
+    void listVaccinesOpenCDXNotAcceptable() {
+        Mockito.when(this.openCDXVaccineRepository.findAllByPatientId(
+                        Mockito.any(OpenCDXIdentifier.class), Mockito.any(Pageable.class)))
+                .thenReturn(new PageImpl<>(
+                        List.of(OpenCDXVaccineModel.builder()
+                                .id(OpenCDXIdentifier.get())
+                                .patientId(OpenCDXIdentifier.get())
+                                .nationalHealthId(UUID.randomUUID().toString())
+                                .build()),
+                        PageRequest.of(1, 10),
+                        1));
+        ListVaccinesRequest request = ListVaccinesRequest.newBuilder()
+                .setPatientId(OpenCDXIdentifier.get().toHexString())
+                .setPagination(Pagination.newBuilder()
+                        .setPageNumber(1)
+                        .setPageSize(10)
+                        .setSortAscending(true)
+                        .setSort("id")
+                        .build())
+                .build();
+        Assertions.assertThrows(OpenCDXNotAcceptable.class, () -> this.openCDXVaccineService.listVaccines(request));
+    }
+
+    @Test
+    void getVaccineById_OpenCDXNotFound() {
+        Mockito.when(this.openCDXVaccineRepository.findById(Mockito.any(OpenCDXIdentifier.class)))
+                .thenAnswer(new Answer<Optional<OpenCDXVaccineModel>>() {
+                    @Override
+                    public Optional<OpenCDXVaccineModel> answer(InvocationOnMock invocation) throws Throwable {
+                        OpenCDXIdentifier argument = invocation.getArgument(0);
+                        return Optional.empty();
+                    }
+                });
+        GetVaccineByIdRequest request = GetVaccineByIdRequest.newBuilder()
+                .setId(ObjectId.get().toHexString())
+                .build();
+        Assertions.assertThrows(OpenCDXNotFound.class, () -> openCDXVaccineService.getVaccineById(request));
+    }
+
+    @Test
+    void listVaccinesOpenCDXNotAcceptable_Sort() {
+        Mockito.when(this.openCDXVaccineRepository.findAllByPatientId(
+                        Mockito.any(OpenCDXIdentifier.class), Mockito.any(Pageable.class)))
+                .thenReturn(new PageImpl<>(
+                        List.of(OpenCDXVaccineModel.builder()
+                                .id(OpenCDXIdentifier.get())
+                                .patientId(OpenCDXIdentifier.get())
+                                .build()),
+                        PageRequest.of(1, 10),
+                        1));
+        ListVaccinesRequest request = ListVaccinesRequest.newBuilder()
+                .setPagination(Pagination.newBuilder()
+                        .setPageNumber(1)
+                        .setPageSize(10)
+                        .setSortAscending(true)
+                        .build())
+                .build();
+        Assertions.assertDoesNotThrow(() -> this.openCDXVaccineService.listVaccines(request));
+    }
+}
