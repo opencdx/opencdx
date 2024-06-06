117a464708dd97b99cf4ac235ebf890a261d944a
Jeffery Miller <jeff@mymiller.name>
2024-06-06 15:18:03 -0400

    NATS to a docker Image

--- deploy.sh
+++ deploy.sh
@@ -503,7 +503,7 @@ build_docker() {
   local auto_select_all=$1
   local auto_confirm_all=$2
 
-  components=("opencdx/mongodb" "opencdx/admin" "opencdx/config" "opencdx/tinkar"
+  components=("opencdx/mongodb" "opencdx/admin" "opencdx/config" "opencdx/tinkar" "opencdx/nats"
     "opencdx/audit" "opencdx/communications" "opencdx/media" "opencdx/health" "opencdx/iam"
     "opencdx/questionnaire" "opencdx/classification" "opencdx/gateway" "opencdx/logistics"
     "opencdx/discovery" "opencdx/graphql")
